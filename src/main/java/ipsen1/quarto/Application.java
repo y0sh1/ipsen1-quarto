983d4f68156516a2b70109c29773c4b8f5a285bd
Mathijs Bernson <mathijs.bernson@gmail.com>
2014-02-11 13:41:00 +0100

    Basic project setup

---
+++ src/main/java/ipsen1/quarto/Application.java
@@ -0,0 +1,10 @@
+package ipsen1.quarto;
+
+/**
+ * Quarto app
+ */
+public class Application {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/ipsen1/quarto/ApplicationTest.java
@@ -0,0 +1,11 @@
+package ipsen1.quarto;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class ApplicationTest {
+    @Test
+    public void testMain() throws Exception {
+        assertTrue(true);
+    }
+}
