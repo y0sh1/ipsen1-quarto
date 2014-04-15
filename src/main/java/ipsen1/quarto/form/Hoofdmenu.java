e54466da4fc6516b3aa21c153369bf7d5d1ba5b4
Y0sh1 <joshua@theinternets.nl>
2014-04-15 15:48:12 +0200

    Changed title font, button styling

--- src/main/java/ipsen1/quarto/form/Hoofdmenu.java
+++ src/main/java/ipsen1/quarto/form/Hoofdmenu.java
@@ -23,7 +23,7 @@ public class Hoofdmenu extends Form {
     private JLabel createTitleLabel() {
         JLabel titleLabel = new JLabel(title, SwingConstants.CENTER);
         titleLabel.setPreferredSize(new Dimension(width, height / 6));
-        titleLabel.setFont(new Font("Comic Sans MS", Font.PLAIN, 48));
+        titleLabel.setFont(new Font("Calibri", Font.PLAIN, 48));
 
         return titleLabel;
     }
@@ -46,11 +46,15 @@ public class Hoofdmenu extends Form {
         int margin = 32;
         buttonsPanel.setLayout(new GridLayout(5, 1, margin, margin));
 
+
         for(int i = 0; i < buttons.length; i++) {
             JButton button = new JButton(buttons[i]);
             TaskListener listener = listeners[i];
 
             button.addActionListener(listener);
+            button.setBackground(Color.BLACK);
+            button.setForeground(Color.WHITE);
+            button.setFont(new Font("Calibri", Font.PLAIN, 24));
             buttonsPanel.add(button);
         }
 
