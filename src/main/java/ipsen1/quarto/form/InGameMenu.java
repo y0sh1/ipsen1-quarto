95a0794b99a5498be91fb2db7f7571ad1f491e6b
Mathijs Bernson <mathijs.bernson@gmail.com>
2014-04-18 13:38:22 +0200

    Verdere uitwerking van het in-game menu

--- src/main/java/ipsen1/quarto/form/InGameMenu.java
+++ src/main/java/ipsen1/quarto/form/InGameMenu.java
@@ -1,6 +1,10 @@
 package ipsen1.quarto.form;
 
 import ipsen1.quarto.QuartoApplication;
+import ipsen1.quarto.task.LaadSpel;
+import ipsen1.quarto.task.LeesInstructies;
+import ipsen1.quarto.task.OpslaanSpel;
+import ipsen1.quarto.task.SluitSpelAf;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,15 +13,76 @@ import java.awt.event.ActionListener;
 
 public class InGameMenu extends Form {
     public InGameMenu() {
-        setPreferredSize(new Dimension(1024, 768));
+        super();
 
-        JButton closeButton = new JButton("Terug");
-        closeButton.addActionListener(new ActionListener() {
+        setPreferredSize(new Dimension(800, 600));
+        setLayout(new GridLayout(7, 1));
+
+        JLabel pauseLabel = new JLabel("Gepauzeerd", SwingConstants.CENTER);
+        pauseLabel.setFont(new Font("Comic Sans MS", Font.PLAIN, 34));
+        add(pauseLabel);
+        addButtons();
+    }
+
+    private JButton
+            resumeButton = new JButton("Spel hervatten"),
+            savebutton = new JButton("Spel opslaan"),
+            loadButton = new JButton("Spel laden"),
+            instructionsButton = new JButton("Instructies"),
+            mainMenuButton = new JButton("Hoofdmenu"),
+            exitButton = new JButton("Afsluiten");
+
+    private void addButtons() {
+        resumeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // Pop the in-game menu
                 QuartoApplication.currentApplication().popForm();
             }
         });
-        add(closeButton);
+
+        savebutton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new OpslaanSpel().run();
+            }
+        });
+
+        loadButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new LaadSpel().run();
+            }
+        });
+
+        instructionsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new LeesInstructies().run();
+            }
+        });
+
+        mainMenuButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Pop the in-game menu and the form underneath it
+                QuartoApplication.currentApplication().popForm();
+                QuartoApplication.currentApplication().popForm();
+            }
+        });
+
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new SluitSpelAf().run();
+            }
+        });
+
+        add(resumeButton);
+        add(savebutton);
+        add(loadButton);
+        add(instructionsButton);
+        add(mainMenuButton);
+        add(exitButton);
     }
 }
