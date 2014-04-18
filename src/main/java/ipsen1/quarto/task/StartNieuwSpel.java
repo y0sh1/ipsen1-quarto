a8e578a6d0925c42314310c8c6f886c6c2b7d904
Mathijs Bernson <mathijs.bernson@gmail.com>
2014-04-18 13:34:06 +0200

    Start alleen een nieuw spel als de speler in het hoofdmenu zit
    Closes #5

--- src/main/java/ipsen1/quarto/task/StartNieuwSpel.java
+++ src/main/java/ipsen1/quarto/task/StartNieuwSpel.java
@@ -2,11 +2,16 @@ package ipsen1.quarto.task;
 
 import ipsen1.quarto.QuartoApplication;
 import ipsen1.quarto.form.BordForm;
+import ipsen1.quarto.form.Hoofdmenu;
 
 public class StartNieuwSpel extends Task {
+    private boolean isInHoofdmenu() {
+        return QuartoApplication.currentApplication().currentForm() instanceof Hoofdmenu;
+    }
+
     @Override
     public boolean validate() {
-        return true;
+        return isInHoofdmenu();
     }
 
     @Override
