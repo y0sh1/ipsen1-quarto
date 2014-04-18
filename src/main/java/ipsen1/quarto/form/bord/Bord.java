c9e57490271e1510aa28f52c461e2550d45c887c
Timmehj <s1083369@student.hsleiden.nl>
2014-04-18 14:26:26 +0200

    ActionListener toegevoegd (moet nog verder uitgewerkt worden)

--- src/main/java/ipsen1/quarto/form/bord/Bord.java
+++ src/main/java/ipsen1/quarto/form/bord/Bord.java
@@ -5,8 +5,10 @@ import ipsen1.quarto.util.QuartoColor;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Bord extends Form {
+public class Bord extends Form implements ActionListener {
     private final int size = 768;
     private JLabel statusLabel = new JLabel("TIJDELIJKE STATUS TEKST. AANPASSEN!!!!");
     private JButton[] knoppen = new JButton[4 * 4];
@@ -43,11 +45,13 @@ public class Bord extends Form {
         for (int i = 0; i < knoppen.length; i++) {
             knoppen[i] = new JButton(VAKJE_ICOON);
             knoppen[i].setPreferredSize(new Dimension(125, 85));
+            knoppen[i].addActionListener(this);
 
             // Haalt de stijl van de JButton weg zodat alleen het rondje over blijft.
             knoppen[i].setContentAreaFilled(false);
             knoppen[i].setBorderPainted(false);
             knoppen[i].setFocusPainted(false);
+
 
             // maakt een paneel aan met een standaard FlowLayout zodat de knoppen kunnen resizen aangezien dat niet mogelijk is met de GridLayout
             JPanel paneel = new JPanel();
@@ -65,5 +69,14 @@ public class Bord extends Form {
         super.paintComponent(g);
         g.setColor(new Color(87, 31, 31));
         g.drawOval(getWidth() / 2 - 300, 125, 600, 600);
+    }
+
+//    Implementeer mij
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton source = (JButton)e.getSource();
+        source.removeActionListener(this);
+
+        System.out.println(source);
     }
 }
