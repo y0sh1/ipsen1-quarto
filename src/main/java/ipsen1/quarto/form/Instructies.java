b8044110cc54a3c63b7279ae98c06a0e404622e0
Y0sh1 <joshua@theinternets.nl>
2014-04-20 00:43:26 +0200

    comments toegevoegd en grote strings iets overzichtelijker

--- src/main/java/ipsen1/quarto/form/Instructies.java
+++ src/main/java/ipsen1/quarto/form/Instructies.java
@@ -32,11 +32,14 @@ public class Instructies extends Form {
         sb.append("- Licht of donker<br>");
         sb.append("- Rond of vierkant<br>");
         sb.append("<br>");
-        sb.append("Aan het begin van elke beurt kan de speler een spelstuk plaatsen op het bord, en vervolgens een spelstuk uitkiezen uitkiezen die de tegenstander tijdens zijn beurt moet plaatsen. Spelstukken die op het bord geplaatst zijn behoren niet tot een bepaalde speler en zijn dus neutraal<br>");
+        sb.append("Aan het begin van elke beurt kan de speler een spelstuk plaatsen op het bord, en vervolgens een spelstuk uitkiezen uitkiezen" +
+                " die de tegenstander tijdens zijn beurt moet plaatsen. Spelstukken die op het bord geplaatst zijn behoren niet tot een" +
+                " bepaalde speler en zijn dus neutraal<br>");
         sb.append("<br><br>");
         sb.append("Doel:<br>");
         sb.append("<br>");
-        sb.append("Het doel van Quarto is, is om 4 op een rij te creëren op het bord met stukken die 1 eigenschap delen, voorbeeld hiervan is 4 lichte spelstukken of 4 ronde spelstukken.");
+        sb.append("Het doel van Quarto is, is om 4 op een rij te creëren op het bord met stukken die 1 eigenschap delen, voorbeeld hiervan is 4 lichte spelstukken" +
+                " of 4 ronde spelstukken.");
         sb.append("Op het moment dat een spelstuk geplaatst is en de speler ziet een rij met gelijke eigenschappen, mag de huidige speler Quarto roepen. " +
                 "Door deze roep te doen, is het mogelijk dat de speler de beurt wint. Als de speler de Quarto niet ziet, kan de tegenstander dit alsnog afroepen, " +
                 "voordat deze zijn gegeven spelstuk plaatst. Mochten beide spelers de Quarto niet zien binnen 1 beurt, zal de Quarto als ongeldig gerekend worden" +
@@ -45,6 +48,7 @@ public class Instructies extends Form {
         sb.append("</HTML>");
         instructieTekst.setText(sb.toString());
 
+        // Knop aanmaken en invullen
         JButton okKnop = new JButton("OK, Terug naar menu");
         okKnop.addActionListener(new ActionListener() {
             @Override
@@ -52,6 +56,7 @@ public class Instructies extends Form {
                 QuartoApplication.currentApplication().popForm();
             }
         });
+
         // Toevoegen Labels
         add(titelLabel);
         add(instructieTekst);
