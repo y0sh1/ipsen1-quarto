1e082c5ef250ff92a3f7c0356048891d3cc110ce
Y0sh1 <joshua@theinternets.nl>
2014-04-19 23:11:28 +0200

    Form aangemaakt, title voor form toegevoegd

--- src/main/java/ipsen1/quarto/form/Instructies.java
+++ src/main/java/ipsen1/quarto/form/Instructies.java
@@ -1,4 +1,17 @@
 package ipsen1.quarto.form;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Instructies extends Form {
+    public Instructies(){
+        super();
+
+        setPreferredSize(new Dimension(800, 600));
+        setLayout(new GridLayout(3,1));
+
+        JLabel titelLabel = new JLabel("Instructies", SwingConstants.CENTER);
+        titelLabel.setFont(new Font("Comic Sans MS", Font.PLAIN, 34));
+        add(titelLabel);
+    }
 }
