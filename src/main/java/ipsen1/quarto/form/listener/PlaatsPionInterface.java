fdf6bed77da10e601fc418f2d6175e5670b65ef7
Mathijs Bernson <mathijs.bernson@gmail.com>
2014-04-20 17:44:23 +0200

    Verwijder wat ongebruikte intefaces

--- src/main/java/ipsen1/quarto/form/listener/PlaatsPionInterface.java
+++
@@ -1,7 +0,0 @@
-package ipsen1.quarto.form.listener;
-
-import ipsen1.quarto.business.Pion;
-
-public interface PlaatsPionInterface {
-    public void plaatsPion(Pion pion); // De pion bevat x- en y-coördinaten
-}
