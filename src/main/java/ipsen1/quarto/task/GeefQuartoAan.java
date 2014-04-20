a19d1842bed3c312a9164433faea0242f6626768
Mathijs Bernson <mathijs.bernson@gmail.com>
2014-04-20 16:41:52 +0200

    Vereenvoudig GeefQuartoAan task

--- src/main/java/ipsen1/quarto/task/GeefQuartoAan.java
+++ src/main/java/ipsen1/quarto/task/GeefQuartoAan.java
@@ -6,27 +6,27 @@ import ipsen1.quarto.form.BordForm;
 public class GeefQuartoAan extends Task {
     private Spel spel;
     private BordForm bordForm;
-    String huidigeSpeler;
 
     public GeefQuartoAan(Spel spel, BordForm bordForm) {
         this.spel = spel;
         this.bordForm = bordForm;
-        huidigeSpeler = spel.getHuidigeSpeler().getNaam();
     }
 
     @Override
     public boolean validate() {
-        if(spel!= null && spel.isQuarto())
-            return true;
-//        Geen geldige Quarto. Geef dit weer en eindig de beurt.
-        spel.volgendeSpeler();
-        bordForm.geefOngeldigeQuartoAan(spel.getStatusText(), huidigeSpeler);
-        return false;
+        return (spel != null) && spel.isQuarto();
     }
 
     @Override
     public void execute() {
-        bordForm.maakWinnaarBekend(huidigeSpeler);
-        spel.spelAfsluiten();
+        String huidigeSpeler = spel.getHuidigeSpeler().getNaam();
+
+        if(spel.isQuarto()) {
+            bordForm.maakWinnaarBekend(huidigeSpeler);
+            spel.spelAfsluiten();
+        } else { // Geen geldige Quarto. Geef dit weer en eindig de beurt.
+            bordForm.geefOngeldigeQuartoAan(spel.getStatusText(), huidigeSpeler);
+            spel.volgendeSpeler();
+        }
     }
 }
