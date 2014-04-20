b6288554a3e06d2939869b59771996afd582e88d
Timmehj <s1083369@student.hsleiden.nl>
2014-04-20 17:10:41 +0200

    KiesSpelersForm begin

---
+++ src/main/java/ipsen1/quarto/form/KiesSpelersForm.java
@@ -0,0 +1,60 @@
+package ipsen1.quarto.form;
+
+import ipsen1.quarto.QuartoApplication;
+import ipsen1.quarto.form.menu.MenuButton;
+import ipsen1.quarto.util.FontOpenSans;
+import ipsen1.quarto.util.QuartoColor;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class KiesSpelersForm extends Form {
+    private static final String BACKGROUND_FILENAME = "src/main/resources/hout_achtergrond.jpg";
+    private static Image background = new ImageIcon(BACKGROUND_FILENAME).getImage();
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(background, 0, 0, null);
+    }
+
+    public KiesSpelersForm() {
+        // Instellen Formaat en layout
+        setPreferredSize(new Dimension(1024, 768));
+        setLayout(new BorderLayout(10, 10));
+        setBackground(QuartoColor.DARK_BROWN);
+        // Defineren Titel koptekst
+        JLabel titelLabel = new JLabel("Geef de namen op", SwingConstants.CENTER);
+        titelLabel.setFont(FontOpenSans.create(48));
+        titelLabel.setForeground(QuartoColor.WHITE);
+
+        // Namen paneel
+        JPanel namenPaneel = new JPanel();
+        JLabel instructieTekst = new JLabel();
+        instructieTekst.setForeground(QuartoColor.WHITE);
+
+        final int side_margin = 1024 / 6;
+        setBorder(new EmptyBorder(0, side_margin, 55, side_margin));
+        JTextField veld = new JTextField();
+
+
+        // Knop aanmaken en invullen
+        JButton okKnop = new MenuButton("OK, Terug naar menu");
+        okKnop.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                QuartoApplication.currentApplication().popForm();
+            }
+        });
+        okKnop.setSize(20,20);
+        okKnop.setBorder(new EmptyBorder(0,0,0,0));
+
+        // Toevoegen Labels
+        add(namenPaneel, BorderLayout.CENTER);
+        add(titelLabel, BorderLayout.NORTH);
+        add(okKnop, BorderLayout.SOUTH);
+    }
+}
--- src/main/java/ipsen1/quarto/task/StartNieuwSpel.java
+++ src/main/java/ipsen1/quarto/task/StartNieuwSpel.java
@@ -4,6 +4,7 @@ import ipsen1.quarto.QuartoApplication;
 import ipsen1.quarto.business.Spel;
 import ipsen1.quarto.form.BordForm;
 import ipsen1.quarto.form.Hoofdmenu;
+import ipsen1.quarto.form.KiesSpelersForm;
 
 import javax.swing.*;
 
@@ -21,12 +22,15 @@ public class StartNieuwSpel extends Task{
     public void execute() {
         Spel spel = new Spel();
         BordForm bord = new BordForm();
-        String[] spelers = new String[2];
-        spelers[0] = JOptionPane.showInputDialog("Vul de naam van Speler 1 in");
-        spelers[1] = JOptionPane.showInputDialog("Vul de naam van Speler 2 in");
-        spel.setSpelers(spelers);
+//        String[] spelers = new String[2];
+//        spelers[0] = JOptionPane.showInputDialog("Vul de naam van Speler 1 in");
+//        spelers[1] = JOptionPane.showInputDialog("Vul de naam van Speler 2 in");
+//        spel.setSpelers(spelers);
+
+
         new SpeelBeurt().execute(spel, bord);
 
         QuartoApplication.currentApplication().presentForm(bord);
+        QuartoApplication.currentApplication().presentForm(new KiesSpelersForm());
     }
 }
