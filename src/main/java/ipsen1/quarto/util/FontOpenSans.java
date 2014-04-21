f8e90978b81065f662b35ed04d938852a343fd6c
Mathijs Bernson <mathijs.bernson@gmail.com>
2014-04-21 10:14:27 +0200

    Verwijder oude font code

--- src/main/java/ipsen1/quarto/util/FontOpenSans.java
+++ src/main/java/ipsen1/quarto/util/FontOpenSans.java
@@ -8,22 +8,4 @@ public class FontOpenSans {
     public static Font create(float size) {
         return new Font("Arial", Font.BOLD, (int) size);
     }
-
-    /*
-    public static Font create(float size) {
-        // Font klasse defineren
-        Font fontOpenSansBase;
-        Font fontOpenSansReal = null;
-        // Font ophalen, koppelen en opblazen
-        try {
-            InputStream is = FontOpenSans.class.getResourceAsStream(new File("/OpenSans-Regular.ttf").getAbsolutePath());
-            fontOpenSansBase = Font.createFont(Font.TRUETYPE_FONT, is);
-
-            fontOpenSansReal = fontOpenSansBase.deriveFont(size);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return fontOpenSansReal;
-    }
-    */
 }
